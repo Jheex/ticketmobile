287685a148e8d08f743ea535cb2c481e3c40130d
Jhonatan Lima <Jhonatanlimacoelho25@gmail.com>
2025-09-17 13:52:20 -0300

    Sidebar almost done

--- Tickets/Models/AppUser.cs
+++
@@ -1,9 +0,0 @@
-namespace tickets.Models
-{
-    public class AppUser
-    {
-        public string Email { get; set; }
-        public string Password { get; set; } // Para teste apenas, sem criptografia
-        public string FullName { get; set; }
-    }
-}
